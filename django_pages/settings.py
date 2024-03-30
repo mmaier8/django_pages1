ba7ca929fe17b72f293005657e9b451121ef7985
Michael Maier <MichaelMaier225@gmail.com>
2024-03-30 13:41:15 -0500

    config

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = "django-insecure-fsg2=l)r9ig6nc=y#7#0tt_0z%55u$fq95!h6!_znh7b1=)wx7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
